1420b7ac3d92e76fa3b17cff9eb804eda0af2696
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2025-01-07 21:40:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ connectDB();
 
 // Configuración CORS
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'http://localhost:5174, http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
 }));
